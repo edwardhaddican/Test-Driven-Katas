32bab1552c2c3a0027ed8f197122f8418662f7c9
Eddie Haddican <edwardhaddican@gmail.com>
2020-03-19 11:30:49 -0400

    working on tests

--- wrap.js
+++ wrap.js
@@ -1,3 +1,38 @@
-const wrap = (line, maxLen) => "";
+const wrap = (line, maxLen) => {
+  let answer = ""
+
+  // if (line.length > maxLen){
+  //   return "Error"
+  // }
+
+  if (line.length <= maxLen){
+    return line
+  }
+
+  if (line.length > maxLen){
+    let lastIndex = line.lastIndexOf(" ")
+    console.log(lastIndex)
+
+    answer += line.slice(0, lastIndex)
+  }
+
+
+
+
+  return answer
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = wrap;
